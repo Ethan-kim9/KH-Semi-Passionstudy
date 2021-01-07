8457ebbc99ef9935530c3c79a52ab21c55330cd4
Saccharin4kg <fpalffldk@gmail.com>
2021-01-07 18:12:33 +0900

    21/01/07

---
+++ passion/src/com/passionStudy/passion/reservation/model/vo/RoomVo.java
@@ -0,0 +1,59 @@
+package com.passionStudy.passion.reservation.model.vo;
+
+public class RoomVo {
+	private int roomNo;
+	private String roomName;
+	private int roomCap;
+	private String roomInfo;
+	private String roomFile;
+	private int roomPrice;
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+
+	public int getRoomCap() {
+		return roomCap;
+	}
+
+	public void setRoomCap(int roomCap) {
+		this.roomCap = roomCap;
+	}
+
+	public String getRoomInfo() {
+		return roomInfo;
+	}
+
+	public void setRoomInfo(String roomInfo) {
+		this.roomInfo = roomInfo;
+	}
+
+	public String getRoomFile() {
+		return roomFile;
+	}
+
+	public void setRoomFile(String roomFile) {
+		this.roomFile = roomFile;
+	}
+
+	public int getRoomPrice() {
+		return roomPrice;
+	}
+
+	public void setRoomPrice(int roomPrice) {
+		this.roomPrice = roomPrice;
+	}
+
+}
